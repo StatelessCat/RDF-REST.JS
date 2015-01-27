20a36a1f93d5b7b31be06c0f29f042ecb7f7263a
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2015-01-27 14:43:39 +0100

    Fix error handling and readability

--- src/parsers/json-ld.js
+++ src/parsers/json-ld.js
@@ -2,27 +2,19 @@
 
 var jsonld = require('jsonld');
 var ntriples = require('./n-triples');
-var Promise = require('promise'); //TODO check why the jslinter don't like
 
-// use the promises API of jsonld
-var promises = jsonld.promises;
+var promises = jsonld.promises; // use the promises API of jsonld
 
 /**
  * Returns a Promise that is able to serve a graph representation
- * @param ntriples
+ * @param
  */
 exports.toGraph = function (jsonld) {
-    return new Promise(function (resolve) {
-        // serialize to RDF
-        var promise = promises.toRDF(jsonld, {format: 'application/nquads'});
-
-        promise.then(function (nquads) {
+    return promises.toRDF(jsonld, {format: 'application/nquads'})
+        .then(function (nquads) {
             console.log(nquads);
             // we have all nquad inside a string, we can get the graph
             var g = ntriples.toGraph(nquads);
-            resolve(g);
-        }, function (err) {
-            console.log(err);
+            return g;
         });
-    });
 };
