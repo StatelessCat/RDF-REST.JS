d5d0ad5698131b6e8d1f03b2409e5d1c3dc84e3a
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2015-02-10 12:46:01 +0100

    a small example of using RDF-REST HTTPCORE on the client side, i need to adapt it for AngularJS

---
+++ examples/core1-clientSide.js
@@ -0,0 +1,22 @@
+/*eslint-env amd, node*/
+
+var getCore = require("../src/cores/factory.js").getCore;
+
+require("../src/parsers/json-ld-adapter"); // ensures that parser is registered
+var jsonld = require("../src/serializers/jsonld.js").jsonld; // ensures that serializer is registered
+
+var bc = getCore("http://localhost:8080/api/user/54d357ea2f6af8e974000001");
+
+bc.getState()
+    .then(function(g) {
+        "use strict";
+        var jsonLD = "";
+        return jsonld(g, function(line) {
+            jsonLD += line;
+        }).then(function() {
+            console.log(jsonLD);
+        });
+    }).catch(function(reason) {
+        "use strict";
+        console.log(reason);
+    });
